5e0d3febf8b4e6c3aa48c1b9dca15176a37eba3f
Jason Baik <baikjs87@gmail.com>
2022-03-27 20:40:46 -0400

    Almost Complete

--- index.js
+++ index.js
@@ -1,33 +1,75 @@
-function forEach() {
-
+function forEach(array, callback) {
+  for (let i = 0; i < array.length; i++) {
+    callback(array[i], i, array)
+  }
 }
 
-function map() {
-
+function map(arr, callback) {
+  const newArr = []
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(callback(arr[i]))
+  }
+  return newArr
 }
 
-function filter() {
-
+function filter(arr, callback) {
+  const newArr = []
+  for (let i = 0; i < arr.length; i++) {
+    if (callback(arr[i]) === true) {
+      newArr.push(arr[i])
+    }
+  }
+  return newArr
 }
 
-function every() {
-
+function every(arr, callback) {
+  for (let i = 0; i < arr.length; i++) {
+    if (filter(arr, callback).length === arr.length) {
+      return true
+    }
+    else {
+      return false
+    }
+  }
 }
 
-function some() {
-
+function some(arr, callback) {
+  for (let i = 0; i < arr.length; i++) {
+    if(filter(arr, callback).length > 0) {
+      return true
+    }
+    else {
+      return false
+    }
+  }
 }
 
-function majority() {
-
+function majority(arr, callback) {
+  for (let i = 0; i < arr.length; i++) {
+    if (filter(arr, callback).length > arr.length/2) {
+      return true
+    }
+    else {
+      return false
+    }
+  }
 }
 
-function once() {
-
+function once(callback) {
+  let test = false
+  if (test) {
+    return callback()
+    test = true
+    once()
+  }
 }
 
-function groupBy() {
-
+function groupBy(arr, callback) {
+  const obj = {}
+  for (let i = 0; i < arr.length; i++) {
+    obj.callback(arr[i]) = arr[i]
+  }
+  return obj
 }
 
 function arrayToObject() {
